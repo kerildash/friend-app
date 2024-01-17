925b5eb452a12d62ef0630400e9f94d199331b1f
kerildash <kerildash@gmail.com>
2024-01-17 17:38:00 +0300

    Add methods to get, edit and delete a friend.

--- MVC/Models/FriendRepository.cs
+++ MVC/Models/FriendRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace MVC.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Models;
 
 public class FriendRepository
 {
@@ -19,8 +21,27 @@ public class FriendRepository
 	{
 		return Friends;
 	}
-	public void Add(Friend friend)
+    public Friend Get(Guid id)
+    {
+		var friend = Friends.Find(x => x.Id == id);
+		return friend is not null ? friend : throw new NullReferenceException();
+    }
+    public void Add(Friend friend)
 	{
 		Friends.Add(friend);
+	}
+
+	public void Edit(Friend friend)
+	{
+		var index = Friends.FindIndex(x => x.Id == friend.Id);
+		if (index == -1)
+		{
+			throw new InvalidOperationException();
+		}
+		Friends[index] = friend;
+	}
+	public void Delete(Guid id)
+	{		
+		Friends.Remove(Get(id));
 	}
 }
